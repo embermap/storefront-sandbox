1d118c8f5d5a6b8c4a2ed8e1c3d65b0fff4ebd9f
Ryan Toronto <ryanto@gmail.com>
2018-12-19 15:25:19 -0500

    Add author, comment, and post models

---
+++ app/models/author.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+
+  posts: DS.hasMany('post'),
+  comments: DS.hasMany('comments')
+});
---
+++ app/models/post.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr('string'),
+  body: DS.attr('string'),
+
+  comments: DS.hasMany('comment'),
+  author: DS.belongsTo('author')
+});
